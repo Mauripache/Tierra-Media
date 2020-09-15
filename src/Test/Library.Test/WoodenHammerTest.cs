a8847d20aa5c919dab815d371fd69232810684a4
Mateone <54219667+Mateone@users.noreply.github.com>
2020-09-15 20:33:47 -0300

    avance test

---
+++ src/Test/Library.Test/WoodenHammerTest.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using MiddleEarth;
+
+namespace Test.Library
+{
+
+
+    public class WoodenHammerTest
+    {
+        int expectedAttack = 100;
+        int expectedDefense = 100;
+        
+        [Test]
+        public void WoodenHammerDefenseCorrect()
+        {
+            WoodenHammer hammer = new WoodenHammer(100, 100);
+            
+        }
+        [Test]
+        public void WoodenHammerDefenseIncorrect()
+        {
+
+        }
+
+    }
+
+
+}
