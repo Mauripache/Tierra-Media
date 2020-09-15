9e22f65583dd8b5c6a3df7832475df95de7a6e39
Mateone <54219667+Mateone@users.noreply.github.com>
2020-09-15 20:55:24 -0300

    git push

--- src/Test/Library.Test/WoodenHammerTest.cs
+++ src/Test/Library.Test/WoodenHammerTest.cs
@@ -8,21 +8,37 @@ namespace Test.Library
     public class WoodenHammerTest
     {
         int expectedAttack = 100;
+        int expectedAttack1 = 0;
         int expectedDefense = 100;
-        
+        int expectedDefense1 = 0;
         [Test]
-        public void WoodenHammerDefenseCorrect()
+        public void WoodenHammerCorrectAttack()
         {
-            WoodenHammer hammer = new WoodenHammer(100, 100);
-            
+            WoodenHammer hammer = new WoodenHammer(15, 15);
+            hammer.Attack = 100;
+            Assert.AreEqual(expectedAttack, hammer.Attack);
         }
         [Test]
-        public void WoodenHammerDefenseIncorrect()
+        public void WoodenHammerIncorrectAttack()
         {
-
+            WoodenHammer hammer = new WoodenHammer(100, 100);
+            hammer.Attack = -1;
+            Assert.AreEqual(expectedAttack1, hammer.Attack);
+        }
+        [Test]
+        public void WoodenHammerCorrectDefense()
+        {
+            WoodenHammer hammer = new WoodenHammer(15, 15);
+            hammer.Defense = 100;
+            Assert.AreEqual(expectedDefense, hammer.Defense);
+        }
+        [Test]
+        public void WoodenHammerIncorrectDefense()
+        {
+            WoodenHammer hammer = new WoodenHammer(100, 100);
+            hammer.Defense = -1;
+            Assert.AreEqual(expectedDefense1, hammer.Defense);
         }
 
     }
-
-
 }
