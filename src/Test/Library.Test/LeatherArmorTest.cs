5bb4aa796328a65484dba97522acb548b3982cba
Mauripache <mauri_pache@hotmail.com>
2020-09-18 00:02:37 -0300

    Added comments for tests

--- src/Test/Library.Test/LeatherArmorTest.cs
+++ src/Test/Library.Test/LeatherArmorTest.cs
@@ -13,24 +13,40 @@ namespace Test.Library
             this.leatherArmor = new LeatherArmor(0, 50);        
         }
         [Test]
+        /*
+            Es necesario probar asignar un valor válido, para asegurar que 
+            no está fallando la implementación del setter del Atributo.
+        */
         public void AssignPositiveAttack()
         {
             this.leatherArmor.Attack = 50;
             Assert.AreEqual(this.leatherArmor.Attack, 50);
         }
         [Test]
+        /*
+            Es necesario probar asignar un valor inválido, para asegurar que 
+            no está fallando la validación del setter del Atributo.
+        */
         public void AssignNegativeAttack()
         {
             this.leatherArmor.Attack = -50;
             Assert.AreEqual(this.leatherArmor.Attack, 0);
         }
         [Test]
+        /*
+            Es necesario probar asignar un valor válido, para asegurar que 
+            no está fallando la implementación del setter del Atributo.
+        */
         public void AssignPositiveDefense()
         {
             this.leatherArmor.Defense = 123;
             Assert.AreEqual(this.leatherArmor.Defense, 123);
         }
         [Test]
+        /*
+            Es necesario probar asignar un valor inválido, para asegurar que 
+            no está fallando la validación del setter del Atributo.
+        */
         public void AssignNegativeDefense()
         {
             this.leatherArmor.Defense = -50;
