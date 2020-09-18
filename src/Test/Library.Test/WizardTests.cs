8f94ed052f94973e4368f9053b90f2a1f5ad925d
Gonzalo Martínez <martinezgonzalo6cel@gmail.com>
2020-09-17 23:17:23 -0300

    Adding Wizard, MagicWand and FireSpear tests

--- src/Test/Library.Test/WizardTests.cs
+++ src/Test/Library.Test/WizardTests.cs
@@ -77,5 +77,37 @@ namespace Test.Library
             Assert.AreEqual(expectedLife, wizard1.Health);
 
         }
+        [Test]
+        public void TestingChangeWand()
+        {
+           Wizard wizard1 = new Wizard("Phoenix"); 
+           wizard1.MagicWand = new MagicWand(30,30);
+           MagicWand newMagicWand = new MagicWand(50,50);
+           wizard1.ChangeWand(newMagicWand);
+           Assert.AreEqual(newMagicWand, wizard1.MagicWand);
+
+        }
+
+        [Test]
+          public void TestingChangeSpear()
+        {
+           Wizard wizard1 = new Wizard("Phoenix"); 
+           wizard1.FireSpear = new FireSpear(30,30);
+           FireSpear newFireSpear = new FireSpear(50,50);
+           wizard1.ChangeSpear(newFireSpear);
+           Assert.AreEqual(newFireSpear, wizard1.FireSpear);
+
+        }
+
+        [Test]
+        public void TestingRemoveWand()
+        {
+            Wizard wizard1 = new Wizard("Phoenix"); 
+            MagicWand wand = new MagicWand(30,30);
+            wizard1.MagicWand = wand;
+            wizard1.RemoveWand(wand);
+            Assert.AreEqual(wizard1.MagicWand,null);
+        }
+
     }
 }
